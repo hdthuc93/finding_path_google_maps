7c700de29e1fc32908cff34a6ef00fa5d82f2c6c
Thuc Huynh <hdthuc93@gmail.com>
2017-04-21 13:33:07 +0700

    add animation for path

---
+++ client/data/point_demo.js
@@ -0,0 +1,147 @@
+
+var destination = {
+    name: "KHTN",
+    lat: 10.763332,
+    lng: 106.682474,
+    adjacentCoordinates: [
+        {
+            // Nguyen Van Cu - An Duong Vuong
+            lat: 10.761139,
+            lng: 106.683378,
+            distance: 263.38222260193055
+        },
+        {
+            // Vong xoay Nguyen Van Cu - Ly Thai To
+            lat: 10.765408,
+            lng: 106.681661,
+            distance: 247.6123924975168
+        }]
+};
+
+var drivers = [
+    {
+        // 67
+        name: "Nguyen Dinh Chieu - Nguyen Thuong Hien",
+        lat: 10.772913,
+        lng: 106.684792,
+        adjacentCoordinates: [
+            {
+                // Dien Bien Phu - Nguyen Thuong Hien
+                lat: 10.775949,
+                lng: 106.682864,
+                distance: 398.33985703208776
+            },
+            {
+                // Nguyen Dinh Chieu - Cao Thang
+                lat: 10.770048,
+                lng: 106.682098,
+                distance: 434.1797061198464
+            },
+            {
+                // Vo Van Tan - Nguyen Thuong Hien
+                lat: 10.771569,
+                lng: 106.685651,
+                distance: 176.65951249971653
+            }]
+    },
+    {
+        // 47
+        name: "Cong Quynh - Nguyen Cu Trinh",
+        lat: 10.764175,
+        lng: 106.690722,
+        adjacentCoordinates: [
+            {
+                // vong xoay Nguyen Trai - Cong Quynh
+                lat: 10.766711,
+                lng: 106.688292,
+                distance: 387.70796214719763
+            },
+            {
+                // Nguyen Cu Trinh - Tran Dinh Xu
+                lat: 10.762918,
+                lng: 106.687695,
+                distance: 359.39478324456417
+            },
+            {
+                // Tran Hung Dao - Nguyen Cu Trinh
+                lat: 10.76481,
+                lng: 106.692474,
+                distance: 204.22356210371495
+            },
+            {
+                // Tran Hung Dao - Cong Quynh
+                lat: 10.763005,
+                lng: 106.69094,
+                distance: 132.4078052849133
+            }]
+    },
+    {
+        // 16
+        name: "Hung Vuong - Le Hong Phong",
+        lat: 10.762183,
+        lng: 106.676458,
+        adjacentCoordinates: [
+            {
+                // vong xoay Ly Thai To - Dien Bien Phu
+                lat: 10.767703,
+                lng: 106.674414,
+                distance: 653.8784170270096
+            },
+            {
+                // Hung Vuong - Tran Nhan Ton
+                lat: 10.761317,
+                lng: 106.675091,
+                distance: 653.8784170270096
+            },
+            {
+                // Tran Phu - Le Hong Phong
+                lat: 10.760513,
+                lng: 106.67703,
+                distance: 196.14598864824876
+            }]
+    },
+    {
+        // 15
+        name: "Tran Hung Dao - Huynh Man Dat",
+        lat: 10.754186,
+        lng: 106.676886,
+        adjacentCoordinates: [
+            {
+                // Tran Hung Dao - Le Hong Phong
+                lat: 10.754774,
+                lng: 106.67895,
+                distance: 278.01998024689055
+            },
+            {
+                // Nguyen Trai - Huynh Man Dat
+                lat: 10.756382,
+                lng: 106.676232,
+                distance: 512.9935166414863
+            }]
+    },
+    {
+        // 56
+        name: "NTMK - CMT8",
+        lat: 10.773654,
+        lng: 106.689452,
+        adjacentCoordinates: [
+            {
+                // NTMK - Ton That Tung
+                lat: 10.771259,
+                lng: 106.6872,
+                distance: 362.94859126944914
+            },
+            {
+                // Vo Van Tan - CMT8
+                lat: 10.774261,
+                lng: 106.688209,
+                distance: 151.79941614447708
+            },
+            {
+                // Bui Thi Xuan - CMT8
+                lat: 10.772708,
+                lng: 106.691223,
+                distance: 220.45112439529402
+            }]
+    },
+];
--- client/js/index.js
+++ client/js/index.js
@@ -1,20 +1,109 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
+var map;
+var lineSymbol;
+var btnFind = document.getElementById("origin-input");;
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         mapTypeControl: false,
         center: {
-            lat: 10.7658943,
-            lng: 106.643373
+            lat: 10.763332,
+            lng: 106.682474
         },
-        zoom: 17
+        zoom: 16
     });
     var infowindow = new google.maps.InfoWindow();
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(10.765889, 106.645567),
+        position: new google.maps.LatLng(10.763332, 106.682474),
         map: map
     });
+
+    lineSymbol = {
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+        scale: 4,
+        strokeColor: 'red'
+    };
+}
+
+
+btnFind.addEventListener('click', function() {
+    for(var i = 0; i < drivers.length; ++i) {
+        findDriver(drivers[i], destination, drawPathInMap);
+    }
+});
+
+function findDriver(start, end, callback) {
+    var result = findShortestPath(start, end);
+    var printNode = result[result.length - 1];
+    console.log("distance: ", printNode.distancePrevNode);
+    var path = [];
+    path.push(end);
+
+    while(printNode.lat !== start.lat && printNode.lng !== start.lng) {
+        path.push(printNode);
+        printNode = findPoint(printNode.prevNode, result);
+    }
+    
+    path.push(start);
+    path.reverse();
+
+    new google.maps.Marker({
+        position: new google.maps.LatLng(start.lat, start.lng),
+        map: map
+    });
+
+    callback(path);
+}
+
+function drawPathInMap(resPath) {
+    var prevNode = resPath[0];
+    for (var i = 1; i < resPath.length; ++i) {
+        var flightPath = new google.maps.Polyline({
+            path: [{ lat: prevNode.lat, lng: prevNode.lng }, { lat: resPath[i].lat, lng: resPath[i].lng }],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+        flightPath.setMap(map);
+        prevNode = resPath[i];
+    }
+    showAnimate(resPath, 0, 1)
+}
+
+function showAnimate(resPath, start, to) {
+    if (to > resPath.length - 1)
+        return;
+
+    var line = new google.maps.Polyline({
+        path: [{lat: resPath[start].lat, lng: resPath[start].lng}, {lat: resPath[to].lat, lng: resPath[to].lng}],
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        icons: [{
+            icon: lineSymbol,
+            offset: '100%'
+        }],
+        map: map
+    });
+
+    animate(line, resPath, to);
+}
+
+function animate(line, resPath, to) {
+    var count = 0;
+    var myVar = window.setInterval(function() {
+        if(count === 199) {
+            clearInterval(myVar);
+            line.set('icons', undefined);
+            return showAnimate(resPath, to, to + 1);
+        }
+
+        count = (count + 1) % 200;
+        var icons = line.get('icons');
+        icons[0].offset = (count / 2) + '%';
+        line.set('icons', icons);
+    }, 10);
 }
